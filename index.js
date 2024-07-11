bff8039c9de922a525c59149fa4d973579b042c8
alex-7-njoroge <njorogekinyanjui02@gmail.com>
2024-07-11 15:58:11 +0300

    lab-3

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
 // Write your code here
+let random = Math.round(Math.random()+1)
+let num3 = 10
+let num4 = 6
+let mod = num3%num4
+let max =Math.max(1,4,7,8,20)
